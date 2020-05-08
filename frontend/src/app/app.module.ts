9576b6a29538f316dacfd0b6ddcfa6a556797720
Marcio Bremer <marcio.bremer@gmail.com>
2020-05-08 15:56:43 -0300

    add button for add product and some routers

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatListModule,
     MatSlideToggleModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/product/product.model.ts
@@ -0,0 +1,5 @@
+export interface Product {
+    id?: number // este simbolo torna o id opcional
+    name: string
+    price: number
+}
